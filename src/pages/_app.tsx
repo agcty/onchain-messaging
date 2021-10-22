98074d565c681bf14792c06c421e2b3d8762df78
Alex Gogl <alex@gogl.io>
2021-10-22 21:38:59 +0100

    add metamask, useModal hook

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,22 @@
 import React from "react"
 
 import { AppProps } from "next/app"
+import { Toaster } from "react-hot-toast"
+
+import { MetamaskContextProvider } from "@hooks/useMetamask"
+import { ModalProvider } from "@hooks/useModal"
+
 import "../styles/tailwind.scss"
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />
+  return (
+    <ModalProvider>
+      <MetamaskContextProvider>
+        <Component {...pageProps} />
+      </MetamaskContextProvider>
+      <Toaster />
+    </ModalProvider>
+  )
 }
 
 export default MyApp

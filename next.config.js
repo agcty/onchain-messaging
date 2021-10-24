5a87b710d377cbcb462f21cf1319869bbed575c7
Alex Gogl <alex@gogl.io>
2021-10-24 07:26:39 +0100

    turn off errors

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  reactStrictMode: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    // ignoreBuildErrors: process.env.DANGEROUSLY_DISREGARD_TYPES === "true",
+    ignoreBuildErrors: true,
+  },
 }
